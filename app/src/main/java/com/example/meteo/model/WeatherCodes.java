42013cea167fe3db1d6b82a5f77d94fd941041c7
Poste IUT <eg581721@INFO-R29-22-06>
2023-05-05 09:35:13 +0200

    Partie 1.2 : ajout du métier au projet

---
+++ app/src/main/java/com/example/meteo/model/WeatherCodes.java
@@ -0,0 +1,14 @@
+package com.example.meteo.model;
+
+/**
+ * Codes for kind of weather
+ */
+public enum WeatherCodes {
+    CLEAR_SKY,
+    PARTIAL_CLOUDED,
+    FOGGY_CLOUDED,
+    SMALL_RAIN,
+    HEAVY_RAIN,
+    SNOW,
+    THUNDERSTORM
+}
